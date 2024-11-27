8567bda586978cbdff20bd2569bf48f77565eb1b
Kshitij Chaturvedi <98537053+Kshitijk14@users.noreply.github.com>
2024-11-27 12:17:17 +0530

    made a utils.py for logging

---
+++ utils.py
@@ -0,0 +1,12 @@
+import logging
+
+def setup_logger(log_file_path):
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(log_file_path),
+            logging.StreamHandler()
+        ]
+    )
+    return logging.getLogger()
